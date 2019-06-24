c5209b7540bf8fa21bb436a95bead445ecd9a81e
rdbarbosa <romulo.brb@gmail.com>
2019-06-24 13:56:30 -0300

    first commit

---
+++ Application/Controllers/UserController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webapicore.Domain.Entities;
+using webapicore.Service.Services;
+using webapicore.Service.Validators;
+
+namespace webapicore.Application.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Usuario")]
+    public class UserController : Controller
+    {
+        private BaseService<User> service = new BaseService<User>();
+
+        [Route("")]
+        public IActionResult Post([FromBody] User item)
+        {
+            try
+            {
+                service.Post<UserValidator>(item);
+
+                return new ObjectResult(item.Id);
+            }
+            catch(ArgumentNullException ex)
+            {
+                return NotFound(ex);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [Route("")]
+        public IActionResult Put([FromBody] User item)
+        {
+            try
+            {
+                service.Put<UserValidator>(item);
+
+                return new ObjectResult(item);
+            }
+            catch(ArgumentNullException ex)
+            {
+                return NotFound(ex);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [Route("")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                service.Delete(id);
+
+                return new NoContentResult();
+            }
+            catch(ArgumentException ex)
+            {
+                return NotFound(ex);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+         [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return new ObjectResult(service.Get());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            try
+            {
+                return new ObjectResult(service.Get(id));
+            }
+            catch(ArgumentException ex)
+            {
+                return NotFound(ex);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+    }
+}
---
+++ Infra/Context/MySqlContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using webapicore.Domain.Entities;
+using webapicore.Infra.Data.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace webapicore.Infra.Data.Context
+{
+    public class MySqlContext : DbContext
+    {
+        public MySqlContext(){}
+        public MySqlContext(DbContextOptions<MySqlContext> options)
+            : base(options){}
+
+        public DbSet<User> User { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+                optionsBuilder.UseSqlServer(@"Server=SES17110\SQLEXPRESS;Database=Db;Trusted_Connection=True;");
+
+                //<add name="CustodiaAtivosContext" connectionString="data source=srvsede0157;initial catalog=CustomizacoesHD;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework" providerName="System.Data.SqlClient" />
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>(new UserMap().Configure);
+        }
+    }
+}
